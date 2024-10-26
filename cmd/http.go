3d8f415299e1d4e5b24e05baa50e23391d992e78
Dicky Pratama <dicky.pratama@arthasemesta.com>
2024-10-26 21:30:00 +0700

    first init commit

---
+++ cmd/http.go
@@ -0,0 +1,46 @@
+package cmd
+
+import (
+	"ewallet-transaction/external"
+	"ewallet-transaction/helpers"
+	"ewallet-transaction/internal/api"
+	"ewallet-transaction/internal/interfaces"
+	"ewallet-transaction/internal/services"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ServeHTTP() {
+	d := dependencyInject()
+
+	r := gin.Default()
+
+	r.GET("/health", d.HealthcheckAPI.HealthcheckHandlerHTTP)
+
+	// walletV1 := r.Group("/trasaction/v1")
+
+	err := r.Run(":" + helpers.GetEnv("PORT", ""))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type Dependency struct {
+	HealthcheckAPI interfaces.IHealthcheckAPI
+	External       interfaces.IExternal
+}
+
+func dependencyInject() Dependency {
+	healthcheckSvc := &services.Healthcheck{}
+	healthcheckAPI := &api.Healthcheck{
+		HealthcheckServices: healthcheckSvc,
+	}
+
+	external := &external.External{}
+
+	return Dependency{
+		HealthcheckAPI: healthcheckAPI,
+		External:       external,
+	}
+}
